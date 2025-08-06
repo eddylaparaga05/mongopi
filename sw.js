f01611449eb9f45ab6f5cf3669cddfd3295eba44
eddylaparaga05 <eddylaparaga05@gmail.com>
2025-08-06 20:14:50 +0800

    Update service worker for GitHub Pages

--- sw.js
+++ sw.js
@@ -1,24 +1,55 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('mongopi-cache').then((cache) => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json',
-        './logo.png',
-        './icon-192.png',
-        './icon-512.png',
-        './admin.html',
-        './kelola-menu.html'
-      ]);
+const CACHE_NAME = 'mongopi-cache-v2';
+const FILES_TO_CACHE = [
+  '/mongopi/',
+  '/mongopi/index.html',
+  '/mongopi/login.html',
+  '/mongopi/admin.html',
+  '/mongopi/kasir.html',
+  '/mongopi/dashboard.html',
+  '/mongopi/kelola-menu.html',
+  '/mongopi/pembukuan.html',
+  '/mongopi/manifest.json',
+  '/mongopi/logo.png',
+  '/mongopi/icon-192.png',
+  '/mongopi/icon-512.png',
+  '/mongopi/notif.mp3'
+];
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+  console.log('[ServiceWorker] Install');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('[ServiceWorker] Caching files');
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
+// Activate Service Worker
+self.addEventListener('activate', (event) => {
+  console.log('[ServiceWorker] Activate');
+  event.waitUntil(
+    caches.keys().then((keyList) =>
+      Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            console.log('[ServiceWorker] Removing old cache', key);
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch files
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
